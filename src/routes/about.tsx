460d908851717ec0366341c7d6c3c64137810931
TheHowdyTooter <thehowdytooter@gmail.com>
2025-11-11 14:16:17 -0500

    we reddy

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -5,5 +5,8 @@ export const Route = createFileRoute('/about')({
 })
 
 function About() {
-  return <div className="p-2">Hello from About!</div>
-}
+  return (
+  <div>
+    This should be an About me Page
+    </div>
+  )}
