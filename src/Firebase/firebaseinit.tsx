3b12dc95632415bb8f879f92cdd5aa1bbe2838bc
TheHowdyTooter <wabrady51@gmail.com>
2025-11-14 15:13:41 -0500

    done firebase init stuff until 'firebase init', follow WEBSITE Instruction

---
+++ src/Firebase/firebaseinit.tsx
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAs5ezsPc22FylZpDBCuiE46NCfDcswMP4",
+
+  authDomain: "physicsrefsite.firebaseapp.com",
+
+  projectId: "physicsrefsite",
+
+  storageBucket: "physicsrefsite.firebasestorage.app",
+
+  messagingSenderId: "347719608640",
+
+  appId: "1:347719608640:web:cfa07039f7d4d5890c72a5"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
