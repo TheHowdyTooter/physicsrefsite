43b19e976b4deeb3bdbd7abbd3c9c251219267ee
TheHowdyTooter <thehowdytooter@gmail.com>
2025-11-14 01:08:17 -0500

    Style work, Equation element final, set up basic site visual structure

--- src/Custom-Elements/Equation-Element.tsx
+++ src/Custom-Elements/Equation-Element.tsx
@@ -11,9 +11,7 @@ export default function Eq({ value }) {
 
     return(
         <>
-            <p className='text'>Equation:</p>
-            <br />
-            <a className='eq'>{Equation}</a>
+            <a className='eq'>{'  ' + Equation + '  '}</a>
         </>
     )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,47 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { useState, useRef } from 'react'
 import Eq from '../Custom-Elements/Equation-Element.tsx';
+import '../styles.css'
+
  
 export const Route = createFileRoute('/')({
   component: Index,
 })
 
 function Index() {
+  const [inputValue, setinputValue] = useState('');
+  const inputRef = useRef(null);
+  const [inputMode, setinputMode] = useState(true);
+
+  const handleSearch = (event: any) => {
+      if (event.key === 'Enter' && inputRef.current) {
+        inputRef.current.blur()
+        setinputMode(false)
+      }
+    };
+
   return (
     <>
-      
-      <head><title>Physics Reference Site</title>Physics Reference</head>
-      <div id ='root' className='background'>
-        <h3 className='text' >Welcome Home!</h3>
-        <br />
-        <br />
-        <Eq value='Ax + By = C'/>
+      <head>
+        <link rel="stylesheet" type='text/css' href="../styles.css" />
+        <title>Physics Reference Site</title>
+      </head>
+      <div>
+
+        <div className='headingBox'>
+          <h3 className='heading' >Are you ready to Physics!!!</h3>
+        </div>
+
+        <div className={(inputMode) ? 'interactiveBoxPre' : 'interactiveBoxPost'}>
+          <input 
+          value={inputValue}
+          className='inputMod' 
+          onKeyDown={handleSearch}
+          onChange={(e) => setinputValue(e.target.value)}
+          ref={inputRef}
+          ></input>
+        </div>
+
       </div>
     </>
   )
